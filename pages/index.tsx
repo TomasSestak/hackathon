4a6224b44cf1e9bf8a299140f2f62788d7e900ad
Tomáš Šesták <tomassestak@seznam.cz>
2021-10-16 19:15:45 +0200

    fix compiled

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import Test from 'https://framer.com/m/Test-ymGv.js@WOc9M8ZCMCxUW5D1ByxE';
 
 const Index: NextPage = () => {
-	return <Test title={'logo'} />;
+	return <></>;
 };
 
 export default Index;
