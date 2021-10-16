f3f11ec6ffbffd0badc2b7c071a3599e8c7afddd
Tomáš Šesták <tomassestak@seznam.cz>
2021-10-16 19:13:15 +0200

    remove framer

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
 	reactStrictMode: true,
-	webpack: (config) => {
-		config.experiments = {
-			buildHttp: true,
-			layers: true,
-		};
-		return config;
-	},
 };
